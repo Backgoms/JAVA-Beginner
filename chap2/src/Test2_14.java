90092302c188faccafd61c453f76d3f9ee479f58
Backgoms <102213232+Backgoms@users.noreply.github.com>
2022-04-27 16:56:48 +0900

    Delete chap2/src directory

--- chap2/src/Test2_14.java
+++
@@ -1,9 +0,0 @@
-
-public class Test2_14 {
-	public static void main(String [] args) {
-		//true 와 false만 넣을 수 있음
-		//직접적으로 쓸일은 많이 없으나 간접적으로 많이 사용
-		boolean aaa1 = true;
-		boolean aaa2 = false;
-	}
-}
